a68a8ae80109b86fee7fbcc7d94adf13eb764f12
muhammadwildan07 <danzaulad@gmail.com>
2023-10-21 01:06:46 +0700

    whitespace

---
+++ WhiteSpace.cpp
@@ -0,0 +1,16 @@
+//
+// Created by wildan on 21/10/23.
+//
+#include <iostream>
+using namespace std;
+
+int main() {
+//    \t	Creates a horizontal tab
+//    \\	Inserts a backslash character (\)
+//    \"	Inserts a double quote character
+    cout << "Hello World! \n\t";
+    cout <<"";
+    cout << "I am learning C++";
+    return 0;
+}
+#include "WhiteSpace.h"
---
+++ WhiteSpace.h
@@ -0,0 +1,14 @@
+//
+// Created by wildan on 21/10/23.
+//
+
+#ifndef UNTITLED_WHITESPACE_H
+#define UNTITLED_WHITESPACE_H
+
+
+class WhiteSpace {
+
+};
+
+
+#endif //UNTITLED_WHITESPACE_H
